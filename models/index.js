9f6a7870526cdeb4a46257450855461899d7479c
BenBasic <benjaminmbasic@gmail.com>
2022-06-24 18:14:05 -0400

    Created Index Model and commented the code inside it

--- models/index.js
+++ models/index.js
@@ -4,14 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Makes the Product table belong to the Category table based on their category_id
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Category table will have one or multiple Product tables based on their category_id
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Creates a relationship between Product and Tag tables while using ProductTag as a join table based on product_id
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Creates a relationship between Tag and Product tables while using ProductTag as a join table based on tag_id
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
+// Exports the Product, Category, Tag, and ProductTag modules to use in other files
 module.exports = {
   Product,
   Category,
